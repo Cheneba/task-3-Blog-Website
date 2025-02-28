0e9637be56778e4f65bca73d34f816bb9b57acc6
Cheneba <chenebametuge14@gmail.com>
2025-02-28 14:13:52 +0100

    put the post data in it's rightful file

---
+++ includes/posts.php
@@ -0,0 +1,72 @@
+<?php
+
+
+$posts_arr = [
+  [
+    "title" => "5 Tips for an Epic White Beard",
+    "Author" => "Mike Lawson",
+    "content" => "1. FIND THE RIGHT WHITE BEARD LENGTH AND SHAPE
+    The longer and more out of control your white beard gets, the more you will have people referring to you as Santa Claus, Merlin, and Gandalf. There’s no way around this — it’s human nature.
+
+    On the flip side, going too short can also be an issue. As your hair loses its pigmentation, it becomes finer. This can leave you with more holes and patches in your beard than you’re accustomed to or a beard that is easy to see through.
+
+    The sweet spot for a white beard tends to be between two and six inches — long enough to provide complete, even coverage but not so long that you enter wizard territory.
+
+    Hard angles are your friend when it comes to shaping a white beard. As we age, our bodies and faces soften and become rounder. You can fight back by opting for a beard style with sharper lines, such as a spade shape or boxed beard.
+
+    2. DOUBLE DOWN ON SOFTENING
+    There’s a common misconception that gray hair is coarser than pigmented hair. But your silver hair is actually finer in texture than it was when you were younger. It only feels coarse and wiry because white hair tends to be drier.
+    ",
+    "date" => "Dec 05, 2023",
+    "image" => ["image1.jpg"],
+  ],
+  [
+
+    "title" => "The Ultimate Guide to Beards and Ingrown Hairs",
+    "author" => "Wil Mouradian",
+    "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. In deleniti labore iste ad, tempora obcaecati ducimus non, soluta commodi nisi modi! Autem nulla sint dolores error velit consequatur placeat animi.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. In deleniti labore iste ad, tempora obcaecati ducimus non, soluta commodi nisi modi! Autem nulla sint dolores error velit consequatur placeat animi.",
+    "date" => "Feb 22, 2018",
+    "image" => ["image2.jpg"],
+  ],
+  [
+
+    "title" => "How To Stop Picking Your Beard and Other Bad Habits",
+    "author" => "Urban Beardsman",
+    "content" => "Why “picking” at your beard all day is bad
+    Fingering your beard can lead to some pretty bad things. First, if you do it enough, you can turn a pretty legit beard into a patchy one. Can you imagine? You grow a beard for months only to have to shave it off because you pick at it too much? That’s what some men may end up doing if they can’t stop plucking at their face fluff. It means starting over.
+
+    How do you stop?
+    Unfortunately, it’s not easy. You first need to know when you’re most likely to pick at it. Because a lot of the time it’s done on an unconscious level. You only realize your fingers are tugging away at individual hairs when you need your hand for something. For me, it’s when my hands are idle. Like, for example, when I’m watching TV.
+
+    Luckily, once you figure out what the trigger is, there are a few options at your disposal to keep your beard looking it’s best.
+
+    Tip #1: Idle Hands
+    One way is to sit on your hands. That way they’re not free to wander. If that doesn’t work, maybe putting them in your pocket will.
+
+    Tip #2: Comb It Out
+    Something else you can try is to keep something with you that’ll occupy those idle hands of yours, like a Beard Comb. You can keep one in your pocket, or at your desk at work. The idea is to have a comb somewhere near you at all times so you can keep those fingers from picking at your whiskers. And in my opinion one of the best combs on the market is made by Beardbrand. It makes combing my curly beard feel like I’m putting a knife through warm butter.
+
+    Tip #3: Pay Up
+    If you don’t think that’ll work, maybe a stress ball would do the trick. Money is also a great motivator. If you make a rule that you must pay your friend/wife/significant other every time you picked at your beard, I’m guessing you’d break that habit pretty damn fast.",
+    "date" => "Mar 30, 2016",
+    "image" => ["image3.jpg"],
+  ],
+  [
+
+    "title" => "Best Beards of Star Wars",
+    "author" => "Urban Beardsman",
+    "content" => "A long time ago, in a galaxy far, far away… To this day, those seminal words still raise the arm hair of men, women, and children across the globe and lucky for us, come December 17th, the entire world will be thrust back into Star Wars pandemonium. Light sabers, adorable droids, and discussions of George Lucas’ dwindling importance to the story will all be discussed around the Christmas dinner table. Kids who might be completely unfamiliar with Anakin Skywalker or Master Yoda will become obsessed with the likes of Finn, Rey, and Poe Dameron overnight. After being exposed to the prequel trilogy at a pivotal age, I feel I can say with some authority, right now is the best time to be a Star Wars fan in the past 30 years.
+
+    Looking back at the incredible spectrum of character we’ve seen in the Star Wars universe over the years, there is one trait that seems to lump certain heroes and villains into one herd, and this time it doesn’t have to do with the force: beards. Good, evil, old, or young (well, young-ish), a certain spectrum of Star Wars characters has taken to longer facial hair in past and present episodes of our favorite space fantasy epic. Sure, Han might’ve taken offense to Leia once calling him “scruffy looking,” but facial hair has been a key aspect for many Star Wars characters over the years, both for those we love and those we loathe
+
+    1. Younger Obi-Wan Kenobi
+    2. Older Obi-Wan Kenobi
+    3. Honorary Mention: Lando Calrissian
+    4. Count Dooku
+    5. Older Luke Skywalker
+    6. Qui-Gon Jinn",
+    "date" => "Dec 17, 2015",
+    "image" => ["image4.jpg"],
+  ],
+];
