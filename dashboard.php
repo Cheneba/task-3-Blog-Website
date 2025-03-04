14cf43a28c81716965f56e8f6304f0cef90ba0d7
Cheneba <chenebametuge14@gmail.com>
2025-03-04 14:27:36 +0100

    Put the code for dashboard, basically displays a summary of all posts and a link to their individual posts. Included the session parameters where needed

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,55 @@
 <?php include "includes/header.php"; ?>
+<?php include "includes/posts.php"; ?>
 
 
+<?php
+
+if (session_status() !== PHP_SESSION_ACTIVE) {
+  session_start();
+}
+if ($_SESSION['status'] !== "logged in") {
+  unset($_SESSION);
+  session_destroy();
+  header("Location: index.php");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h2><?php echo $_SESSION['username'] ?: "User" ?></h2>
+  <br>
+  <hr>
+  <h3>Hello, <?php echo $_SESSION['message'] ?: "welcome" ?>
+  </h3>
 
 
+  <br>
+  <hr>
+  <?php foreach ($posts_arr as $post):
+
+  ?>
+
+    <div class="dashboard-post">
+      <h2><?php echo $post['title'] ?></h2>
+      <br>
+      <p><?php echo $post['description'] ?></p>
+      <br>
+      <a href="post.php?id=<?php echo base64_encode($post["id"]); ?>">View more</a>
+      <br>
+      <br>
+      <hr>
+    </div>
+  <?php endforeach; ?>
+</body>
+
+</html>
 
 <?php include "includes/footer.php"; ?>
